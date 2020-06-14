c1a54691a4e49407ab2d584da6f44f13a89584c7
anteastra <anteastra@gmail.com>
2020-06-14 18:41:26 +0200

    Show first meme on startup page

---
+++ java/src/main/java/com/anteastra/memshmem/MemShmemApplication.java
@@ -0,0 +1,13 @@
+package com.anteastra.memshmem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemShmemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemShmemApplication.class, args);
+	}
+
+}
---
+++ java/src/test/java/com/anteastra/memshmem/MemShmemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anteastra.memshmem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemShmemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
